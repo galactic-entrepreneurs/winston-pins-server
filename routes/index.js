582b1f393a5652db9928be3c11bbd13920632e08
AkashGutha <akash.gutha@outlook.com>
2016-12-20 06:55:39 +0530

    sql query changes

--- routes/index.js
+++ routes/index.js
@@ -5,40 +5,43 @@ var pool = require('../pool').pool;
 /* GET home page. */
 router.get('/streets', function (req, res, next) {
   var q = req.query;
-  var sqlQuery = 
-    `SELECT * FROM 
-    gnaf_201611.streets as g , gnaf_201611.address_aliases
-    WHERE ST_Contains(
-    ST_GeomFromEWKT('POLYGON((` + q.turf + `))'),
-    ST_MakePoint(g.longitude,g.latitude))
-    LIMIT 100`;  
-  
-// to run a query we can acquire a client from the pool,
-// run a query on the client, and then return the client to the pool
+  var sqlQuery =
+    "SELECT a.address FROM" +
+    " gnaf_201611.streets as g ," +
+    " gnaf_201611.address_aliases as a" +
+    " WHERE ST_Contains(  ST_GeomFromEWKT('POLYGON((" +
+    q.turf +
+    "))',ST_MakePoint(g.longitude,g.latitude))" +
+    " AND " +
+    " a.gid = g.gid;";
+
+  // to run a query we can acquire a client from the pool,
+  // run a query on the client, and then return the client to the pool
   pool.connect(function (err, client, done) {
-    
-    if(err) {
+
+    if (err) {
       return console.error('error fetching client from pool', err);
     }
 
-    client.query( sqlQuery , function (err, result) {
+    client.query(sqlQuery, function (err, result) {
       //err is the error returned from the PostgreSQL server
       //handle the error here
-      
+      console.log(sqlQuery);
+
       if (err) {
         return console.error('error running query', err);
       }
 
       console.log(result);
-      console.log(sqlQuery);
 
       if (result) {
-        res.send(result); return;
+        res.send(result);
+        return;
       }
-        //output: 1
-      });
-    
-});
+      //output: 1
+    });
+
+  });
 
   //res.send(sqlQuery);
 });
